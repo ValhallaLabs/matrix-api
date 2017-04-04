36a3b4fb41f35b038382f448bfd10ce3353ef3cb
vboitsov <vadimboitsov1@gmail.com>
2017-04-04 14:28:05 +0300

    change set to linked hash set in sync model

--- src/main/java/ua/softgroup/matrix/api/model/datamodels/SynchronizationModel.java
+++ src/main/java/ua/softgroup/matrix/api/model/datamodels/SynchronizationModel.java
@@ -1,7 +1,7 @@
 package ua.softgroup.matrix.api.model.datamodels;
 
 import java.io.Serializable;
-import java.util.Set;
+import java.util.LinkedHashSet;
 
 /**
  * @author Vadim Boitsov <sg.vadimbojcov@gmail.com>
@@ -9,17 +9,17 @@ import java.util.Set;
 public class SynchronizationModel implements Serializable, DataModel {
     private static final long serialVersionUID = 1L;
 
-    private Set<CheckPointModel> checkPointModels;
+    private LinkedHashSet<CheckPointModel> checkPointModels;
 
-    public SynchronizationModel(Set<CheckPointModel> checkPointModels, long idleTime) {
+    public SynchronizationModel(LinkedHashSet<CheckPointModel> checkPointModels) {
         this.checkPointModels = checkPointModels;
     }
 
-    public Set<CheckPointModel> getCheckPointModels() {
+    public LinkedHashSet<CheckPointModel> getCheckPointModels() {
         return checkPointModels;
     }
 
-    public void setCheckPointModels(Set<CheckPointModel> checkPointModels) {
+    public void setCheckPointModels(LinkedHashSet<CheckPointModel> checkPointModels) {
         this.checkPointModels = checkPointModels;
     }
 
